6fa23cc75de15efd1e0b6e1c2b294ff9405638be
SanZenSekai <45728342+Sanzensekai-mx@users.noreply.github.com>
2023-02-15 21:17:06 +0400

    templates, views for Post

--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+
+class ListPosts(ListView):
+    model = Post
+    template_name = 'post_news.html'
+    context_object_name = 'post_news'
+
+
+class DetailPosts(DetailView):
+    model = Post
+    template_name = 'post_new_detail.html'
+    context_object_name = 'post_new'
